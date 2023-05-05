03b73afe9a6fa9d2256553d89f8c580985144bf7
stgkrt <taro.stst@gmail.com>
2023-05-05 14:36:52 +0900

    add docs

--- my_utils/sph_main.py
+++ my_utils/sph_main.py
@@ -3,15 +3,34 @@ def add_number(a, b):
 
     説明の追加は日本語でもいける？
     なんかいろいろ書いてもよさそう
-    TODO:これとか同じ感じで入る？
-
-    NOTE: これはノートです
-
+    Todo:
+        Todo.これとか同じ感じで入る？
+    
     Args:
         a (int): first number...
         b (int): second number
 
     Returns:
         int: sum of a and b
+
+    Notes: 
+        これはノートです
+    
+    Note:
+        これもノートです
+
+    Warnings: 
+        これはワーニングです
+
+    Raises:
+        ValueError: aとbはintである必要があります
+
+    Examples:
+        >>> add_number(1,2)
+
+    Tip:
+        https://www.sphinx-doc.org/ja/master/usage/extensions/napoleon.html
     """
+    if not isinstance(a, int) or not isinstance(b, int):
+        raise ValueError("a and b must be int")
     return a + b
