fb09348cfc2a83a631fed852015a1405a7fde33d
stgkrt <taro.stst@gmail.com>
2023-05-05 14:25:55 +0900

    add docs

--- my_utils/sph_main.py
+++ my_utils/sph_main.py
@@ -1,6 +1,12 @@
 def add_number(a, b):
     """ simpe add function
 
+    説明の追加は日本語でもいける？
+    なんかいろいろ書いてもよさそう
+    TODO:これとか同じ感じで入る？
+
+    NOTE: これはノートです
+
     Args:
         a (int): first number...
         b (int): second number
