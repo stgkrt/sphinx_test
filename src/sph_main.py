e8a444e056f68847f855300794a655b68ca37ef0
stgkrt <taro.stst@gmail.com>
2023-05-05 12:15:28 +0900

    add for doc fils

---
+++ src/sph_main.py
@@ -0,0 +1,8 @@
+def add_number(a, b):
+    """Add two numbers together
+    
+    Args:a (int): first number  
+    Args:b (int): second number
+    return: sum of a and b
+    """
+    return a + b
